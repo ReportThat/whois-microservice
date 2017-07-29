f1afa64b6008e46f8a61acdc12a9ce61e8e0304e
ugiacoman <ulises.giacoman@forrent.com>
2017-07-29 10:51:45 -0400

    Clean

--- index.js
+++ index.js
@@ -41,33 +41,5 @@ const whoIs = (request, response) => {
   })
 }
 
-//
-//
-// const pingSlack = async (request, response)  => {
-//   const { query } = parse(request.url, true)
-//
-//   var payload = { 'channel': process.env.CHANNEL,
-//                     'username': query.username,
-//                     'text': query.text,
-//                     'icon_emoji': ':' + query.emoji + ':'
-//                   }
-//   const formBody = ['payload=' + JSON.stringify(payload)]
-//
-//   const response = await fetch(process.env.SLACK_URL, {
-//                                 method: "POST",
-//                                 headers: {
-//                                   "cache-control": "no-cache",
-//                                   "content-type": "application/x-www-form-urlencoded"
-//                                 },
-//                                 body: formBody
-//                                 })
-//                               .then(function(response) {
-//                                 return(response.text())
-//                               }, function(error) {
-//                                 return('Couldn\'t reach slack, check if you configured your .env file correctly.')
-//                               })
-// return response
-// }
-
-const cors = microCors({ allowMethods: ['GET']})
+const cors = microCors({allowMethods: ['GET']})
 module.exports = cors(whoIs)
